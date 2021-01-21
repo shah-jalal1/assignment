15cb7c63494fcd4943a4834bb0d8865aff003124
shahjalalcse <shah-jalal1>
2021-01-21 23:35:44 +0600

    2nd problem complete:

--- assignment.js
+++ assignment.js
@@ -1,13 +1,30 @@
 // PROBLEM 1
-function kilometerToMeter(km){
-    if(km < 0){
-        return "Negative input is not acceceptable"
+function kilometerToMeter(km) {
+    if (km < 0) {
+        return "Negative input is not acceceptable";
     }
-    else{
+    else {
         meter = 1000 * km;
         return meter;
-    } 
+    }
 }
 var result1 = kilometerToMeter(5);
 console.log("Result is:", result1);
 
+
+
+// PROBLEM 1
+function budgetCalculator(watch, phone, laptop) {
+    if(watch < 0 || phone < 0 || laptop < 0){
+        return "Negative input is not acceceptable";
+    }
+    else {
+        watchPrice = 50 * watch;
+        phonePrice = 100 * phone;
+        laptopPrice = 500 * laptop;
+        totalBudget = watchPrice + phonePrice + laptopPrice;
+        return totalBudget
+    }
+}
+var budget = budgetCalculator(2, 2, 2);
+console.log("Total budget:", budget);
